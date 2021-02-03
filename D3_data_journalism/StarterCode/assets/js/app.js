21e08c7ed65633e56808c25f66e7ca56d4a3f4e3
Loic Tiemani <loictiems@github.com>
2021-02-03 18:29:54 -0500

    c

---
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -0,0 +1,28 @@
+// @TODO: YOUR CODE HERE!
+
+
+var svgWidth = 960;
+var svgHeight = 500;
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var parseTime = d3.timeParse("%d-%b")
+d3.csv("./data.csv").then(function(donutData){
+    donutData.forEach(function(dataPoint) {
+        dataPoint.date = parseTime(dataPoint.date);
+        dataPoint.morning = +dataPoint.morning;
+        dataPoint.evening = +dataPoint.evening;
+      });
+    console.log(donutData);vv
