48c4bb8304f3f184c7afb9bb1b486bd1a41e683d
Loic Tiemani <loictiems@github.com>
2021-02-03 19:07:11 -0500

    Update app.js

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -31,11 +31,13 @@ d3.csv("./assets/data/data.csv").then(function(Data){
     Data.forEach(function(dataPoint) {
       // Parser through the data and cast 
         dataPoint.poverty = +dataPoint.date;
+
         dataPoint.obesityLow = +dataPoint.obesityLow;
+        dataPoint.obesityHigh = +dataPoint.obesityHigh;
         dataPoint.obesityHigh = +dataPoint.obesityHigh;
       });
     console.log(Data);
- })
+ })  
     // id,state,abbr,poverty,povertyMoe,age,ageMoe,
     // income,incomeMoe,healthcare,healthcareLow,
     // healthcareHigh,obesity,obesityLow,obesityHigh,smokes,smokesLow,smokesHigh
