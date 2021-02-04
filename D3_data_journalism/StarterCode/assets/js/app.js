effe4a2599be310c0bec31f18fab92609f2410a1
Loic Tiemani <loictiems@github.com>
2021-02-03 19:05:02 -0500

    Update app.js

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -11,22 +11,31 @@ var margin = {
 };
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
 var svg = d3
   .select("body")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
+
+  // Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
-var parseTime = d3.timeParse("%d-%b")
-d3.csv("./data.csv").then(function(Data){
-    Data.forEach(function(dataPoint) {
-        dataPoint.date = parseTime(dataPoint.date);
-        dataPoint.morning = +dataPoint.morning;
-        dataPoint.evening = +dataPoint.evening;
-      });
-    console.log(donutData);vv
 
-    id,state,abbr,poverty,povertyMoe,age,ageMoe,
-    income,incomeMoe,healthcare,healthcareLow,
-    healthcareHigh,obesity,obesityLow,obesityHigh,smokes,smokesLow,smokesHigh
+
+
+ //Retrieve data from the CSV file and execute everything below
+d3.csv("./assets/data/data.csv").then(function(Data){
+    Data.forEach(function(dataPoint) {
+      // Parser through the data and cast 
+        dataPoint.poverty = +dataPoint.date;
+        dataPoint.obesityLow = +dataPoint.obesityLow;
+        dataPoint.obesityHigh = +dataPoint.obesityHigh;
+      });
+    console.log(Data);
+ })
+    // id,state,abbr,poverty,povertyMoe,age,ageMoe,
+    // income,incomeMoe,healthcare,healthcareLow,
+    // healthcareHigh,obesity,obesityLow,obesityHigh,smokes,smokesLow,smokesHigh
